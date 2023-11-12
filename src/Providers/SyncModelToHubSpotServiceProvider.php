fb865a0bdc8b7707e498335c64b262e1699174dc
Warren Coetzee <warren.coetzee@gmail.com>
2023-11-12 20:00:26 +0800

    alpha-develop: Added blank Laravel provider and Updated composer.json accordingly

---
+++ src/Providers/SyncModelToHubSpotServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Wazza\SyncModelToHubspot\Providers;
+
+use Illuminate\Support\ServiceProvider as BaseServiceProvider;
+
+class SyncModelToHubSpotServiceProvider extends BaseServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
